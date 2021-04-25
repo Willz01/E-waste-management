2c1c5e9952cbef89b724f18f999b03d74635295c
Fanny <fannysarasoderlund@gmail.com>
2021-04-25 14:36:40 +0200

    added navigation and 2 fragments

---
+++ app/src/main/java/dev/samuelmcmurray/e_wastemanagement/HomeFragment.kt
@@ -0,0 +1,17 @@
+package dev.samuelmcmurray.e_wastemanagement
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment(){
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.home_fragment, container, false)
+
+}
