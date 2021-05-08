32346d8dd06354fcb1e410df83ea33a1172c8368
Fanny <fannysarasoderlund@gmail.com>
2021-05-08 22:17:43 +0200

    correction of tests

--- app/src/androidTest/java/dev/samuelmcmurray/e_wastemanagement/FireBaseTests.kt
+++ app/src/androidTest/java/dev/samuelmcmurray/e_wastemanagement/FireBaseTests.kt
@@ -1,37 +1,68 @@
 package dev.samuelmcmurray.e_wastemanagement
+
 import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dev.samuelmcmurray.e_wastemanagement.data.model.Item
+import dev.samuelmcmurray.e_wastemanagement.utils.ItemUtils
+import dev.samuelmcmurray.e_wastemanagement.utils.ItemsCallback
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.Assert.*
 
+
 @RunWith(AndroidJUnit4::class)
 class FireBaseTests {
-    val appContext: Context = InstrumentationRegistry.getInstrumentation().getTargetContext()
+
+    val appContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
 
     val itemUtils = ItemUtils()
 
+    val testItem1 = Item(
+        "Samsung A2",
+        "zs11sa",
+        "2315",
+        "11231s",
+        "1955",
+        null,
+        null,
+        null,
+        null,
+        "samsung",
+        "very nice thing"
+    )
 
     @Test
     fun useAppContext() {
-        assertEquals("dev.samuelmcmurray.e_wastemanagement", appContext.getPackageName())
+        assertEquals("dev.samuelmcmurray.e_wastemanagement", appContext.packageName)
     }
 
     @Test
     fun addToDB() {
-        val testItem1 = Item(
-            "Samsung A2",
-            "zs11sa",
-            "11231s",
-            null,
-            "Samsung",
-            null,
-            "Nice phone with camera issue"
-        )
         val result = itemUtils.addItemToFirebase(testItem1)
 
         assertEquals(true, result)
     }
+
+    @Test
+    fun addItemToFirebase() {
+        assertTrue(
+            ItemUtils.newInstance()
+                .addItemToFirebase(testItem1)
+        )
+    }
+
+    @Test
+    fun readItemsFromFirebase() {
+        assertTrue(
+            ItemUtils.newInstance()
+                .readItemsFromFirebase(object :
+                    ItemsCallback {
+                    override fun onCallback(value: ArrayList<Item>) {
+                    }
+                })
+        )
+    }
+
 
 }
